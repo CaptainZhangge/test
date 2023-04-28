aa1655a5f1c97e6134f040b9895e2395c3b296c1
“xxx” <xxx>
2023-04-28 20:31:48 +0800

    up

--- qspi_prj/tb/tb_asyn_fifo.sv
+++ qspi_prj/tb/tb_asyn_fifo.sv
@@ -1,7 +1,11 @@
 `timescale 1ns/1ps
 `define    clk_period	10
 `define    SIM_TIMEOUT  1000000
+
 module tb_asyn_fifo();
+
+parameter					DEEPTH = 4;
+parameter					DATA_WIDTH = 2;
 
 reg							w_clk;
 reg	 						w_rst_n;
@@ -45,6 +49,13 @@ r_en <= 1'd1;
 #(`clk_period)
 r_en <= 1'd0;
 #100;
+w_en <= 1'd1;
+wdata <= 2'd2;
+r_en <= 1'd1;
+#(`clk_period)
+w_en <= 1'd0;
+r_en <= 1'd0;
+#1000;
 $finish(2);
 end
 
@@ -68,9 +79,18 @@ initial begin
 end
 
 initial begin
-	$fsdbDumpfile("asyn_fifo.fsdb");
-	$fsdbDumpvars("+all");
+//if($test$plusargs("DUMP_FSDB"))
+begin
+	$fsdbDumpfile("test_tb.fsdb");  //记录波形，波形名字testname.fsdb
+	$fsdbDumpvars("+all");  //+all参数，dump SV中的struct结构体
+	$fsdbDumpSVA();   //将assertion的结果存在fsdb中
+	$fsdbDumpMDA(0, tb_asyn_fifo);  //dump memory arrays
+	//0: 当前级及其下面所有层级，如top.A, top.A.a，所有在top下面的多维数组均会被dump
+	//1: 仅仅dump当前组，也就是说，只dump top这一层的多维数组。
+	#(`SIM_TIMEOUT);
 	$finish(2);
 end
+end
+
 
 endmodule
